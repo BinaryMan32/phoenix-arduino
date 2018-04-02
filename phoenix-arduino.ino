ba0a087436e6e569a183a86bf2a3b48eb615c661
Fred Fettinger <fettinge@gmail.com>
2018-04-02 01:49:12 -0500

    test reading potentiometers with analog inputs
    To demo reading the analogs potentiometers continuously and still be able
    to see button presses, skip printing the analogs while a button is down.

--- phoenix-arduino.ino
+++ phoenix-arduino.ino
@@ -22,7 +22,7 @@
 
 /* Optional delay between shift register reads.
 */
-#define POLL_DELAY_MSEC   1
+#define POLL_DELAY_MSEC   10
 
 const size_t numButtonGroups = 3;
 
@@ -59,6 +59,48 @@ void setup()
     hc165_collection_setup(hc165_collection);
     hc165_collection_read(hc165_collection);
     hc165_collection_print(hc165_collection);
+
+    analogReference(DEFAULT);
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A8, INPUT);
+    pinMode(A9, INPUT);
+}
+
+/*
+ * Attempting to read the voltage across only a potentiometer will always
+ * return 1023. The voltage difference is 5V because there is nothing else in
+ * the circuit. To measure the resistance, we use a reference resistor in
+ * series with the potentiometer:
+ *
+ *  5V --v^v^v-- V1 --v^v^v-- GROUND
+ *        Rpot         Rref
+ *
+ * To solve for Rpot, use Ohm's law and the fact that 2 resistors in series
+ * must have the same current:
+ *
+ *  (Vsupply - V1) / Rpot = (V1 - 0V) / Rref
+ *  (Vsupply - V1) * Rref = V1 * Rpot
+ *  (Vsupply / V1 - 1) * Rref = Rpot
+ *  Vsupply * Rref / V1 - Rref = Rpot
+ *
+ * The ADC maps 5V (assumed to be both the ADC reference voltage and the supply
+ * voltage) to a value 0 - 1023 (kAdcOutputMax). Since only the ratio of
+ * Vsupply and V1 is important, we can treat both as ADC outputs instead of
+ * converting from ADC to voltage on both sides.
+ *
+ * See http://www.built-to-spec.com/blog/2009/09/10/using-a-pc-joystick-with-the-arduino/
+ */
+const long kDivResistanceKOhm = 100;
+const long kAdcOutputMax = 1023;
+
+uint8_t get_potentiometer_resistance(int adc_in)
+{
+  if(adc_in > 0) {
+    return uint8_t((kAdcOutputMax * kDivResistanceKOhm) / adc_in - kDivResistanceKOhm);
+  } else {
+    return 0;
+  }
 }
 
 void loop()
@@ -66,6 +108,26 @@ void loop()
     hc165_collection_read(hc165_collection);
     if(hc165_collection_changed(hc165_collection)) {
         hc165_collection_print(hc165_collection);
+    }
+
+    bool isButtonPressed = false;
+    for(int i = 0; i < hc165_collection.size; i++) {
+        if(hc165_data[i].values != 0xff) {
+            isButtonPressed = true;
+            break;
+        }
+    }
+
+    if(!isButtonPressed) {
+        Serial.print("Axes: x:");
+        Serial.print(get_potentiometer_resistance(analogRead(A0)));
+        Serial.print(" y:");
+        Serial.print(get_potentiometer_resistance(analogRead(A1)));
+        Serial.print(" r:");
+        Serial.print(get_potentiometer_resistance(analogRead(A8)));
+        Serial.print(" t:");
+        Serial.print(get_potentiometer_resistance(analogRead(A9)));
+        Serial.print("\r\n");
     }
 
     delay(POLL_DELAY_MSEC);
