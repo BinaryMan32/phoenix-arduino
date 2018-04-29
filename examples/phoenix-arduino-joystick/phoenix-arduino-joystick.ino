76ec3dc5f8fb79dbd7ab2a3486e1a5f000035b14
Fred Fettinger <fettinge@gmail.com>
2018-04-29 16:48:04 -0500

    examples: add phoenix-arduino-joystick
    Create another example which should enumerate as a USB HID Joystick on the
    attached computer and send the axes and button states in a report message.
    
    This example works only on Arduino devices with HID support. These devices
    use the main processor to access USB instead of a separate processor, which
    allows them to provide additional USB endpoints that can be accessed
    directly by the implementation.

---
+++ examples/phoenix-arduino-joystick/phoenix-arduino-joystick.ino
@@ -0,0 +1,151 @@
+#include <HID.h>
+
+#include "hc165.h"
+#include "Potentiometer.h"
+#include "HidDescriptorHelper.h"
+
+const size_t kNumAxes = 4;
+using AxesValueType = int16_t;
+const AxesValueType kAxesMin = -32768;
+const AxesValueType kAxesMax = 32767;
+
+const size_t kNumButtonGroups = 3;
+
+// Report data sent by the HID input device
+struct ReportData {
+  AxesValueType axes[kNumAxes];
+  u8 buttons[kNumButtonGroups];
+};
+
+using namespace usb::hid;
+
+const u8 kReportId = 1;
+static const u8 sHidDescriptorData[] PROGMEM = {
+  Global::UsagePage | 1, usage::Page::GenericDesktop,
+  Local::Usage | 1, usage::generic_desktop::Application::Joystick,
+
+  Main::Collection | 1, Collection::Application,
+    Global::ReportID | 1, kReportId,
+
+    /* 4 16bit axes */
+    Main::Collection | 1, Collection::Physical,
+      Global::UsagePage | 1, usage::Page::GenericDesktop,
+      Local::Usage | 1, usage::generic_desktop::Axis::X,
+      Local::Usage | 1, usage::generic_desktop::Axis::Y,
+      Local::Usage | 1, usage::generic_desktop::Axis::Rx,
+      Local::Usage | 1, usage::generic_desktop::Axis::Z,
+      Global::LogicalMinimum | 2, 0x00, 0x80, // kAxesMin = -32768
+      Global::LogicalMaximum | 2, 0xFF, 0x7F, // kAxesMax = 32767
+      Global::ReportSize | 1, 16,
+      Global::ReportCount | 1, 4,
+      Main::Input | 1, DataBits::Variable,
+    Main::EndCollection | 0,
+
+    // 24 1 bit buttons
+    Global::UsagePage | 1, usage::Page::Button,
+    Local::UsageMinimum | 1, 1,
+    Local::UsageMaximum | 1, 24,
+    Global::LogicalMinimum | 1, 0,
+    Global::LogicalMaximum | 1, 1,
+    Global::ReportSize | 1, 1,
+    Global::ReportCount | 1, 24,
+    Main::Input | 1, DataBits::Variable,
+
+  Main::EndCollection | 0,
+};
+
+#define POLL_DELAY_MSEC   1
+
+const hc165_config_t hc165_configs[kNumButtonGroups] = {
+  {
+    .name = "handle",
+    .ploadPin = 11,
+    .clockDataPin = 12,
+  },
+  {
+    .name = "  wing",
+    .ploadPin = 3,
+    .clockDataPin = 2,
+  },
+  {
+    .name = "thrttl",
+    .ploadPin = 7,
+    .clockDataPin = 6,
+  },
+};
+
+hc165_data_t hc165_data[kNumButtonGroups];
+
+hc165_collection_t hc165_collection = {
+  .config = hc165_configs,
+  .data = hc165_data,
+  .size = kNumButtonGroups,
+};
+
+const Potentiometer axesPotentiometers[kNumAxes] = {
+  { Potentiometer::Parameters{
+      .name = "X",
+      .analogInputPin = A0,
+      .referenceResistance = 100,
+      .resistanceRange = {.min = 0, .max = 87},
+      .outputRange = {.min = kAxesMin, .max = kAxesMax},
+    }
+  },
+  { Potentiometer::Parameters{
+      .name = "Y",
+      .analogInputPin = A1,
+      .referenceResistance = 100,
+      .resistanceRange = {.min = 0, .max = 85},
+      .outputRange = {.min = kAxesMin, .max = kAxesMax},
+    }
+  },
+  { Potentiometer::Parameters{
+      .name = "R",
+      .analogInputPin = A4,
+      .referenceResistance = 100,
+      .resistanceRange = {.min = 0, .max = 94},
+      .outputRange = {.min = kAxesMin, .max = kAxesMax},
+    }
+  },
+  { Potentiometer::Parameters{
+      .name = "T",
+      .analogInputPin = A5,
+      .referenceResistance = 100,
+      .resistanceRange = {.min = 0, .max = 92},
+      .outputRange = {.min = kAxesMin, .max = kAxesMax},
+    }
+  },
+};
+
+void setup()
+{
+  hc165_collection_setup(hc165_collection);
+
+  // Add USB HID device description of the Phoenix
+  static HIDSubDescriptor sHidDescriptior(sHidDescriptorData, sizeof(sHidDescriptorData));
+  HID().AppendDescriptor(&sHidDescriptior);
+}
+
+void loop()
+{
+  ReportData reportData;
+
+  // Read axes
+  for (int i = 0; i < kNumAxes; i++) {
+    reportData.axes[i] = axesPotentiometers[i].read();
+  }
+
+  // Reverse the throttle direction since min resistance is at the bottom
+  // todo: make Potentiometer clamping handle to enable:
+  //   .outputRange = {.min = kAxesMax, .max = kAxesMin},
+  reportData.axes[3] = map(reportData.axes[3], kAxesMin, kAxesMax, kAxesMax, kAxesMin);
+
+  // Read buttons
+  hc165_collection_read(hc165_collection);
+  for (int i = 0; i < kNumButtonGroups; i++) {
+    reportData.buttons[i] = hc165_data[i].values;
+  }
+
+  // Report data to host
+  HID().SendReport(kReportId, &reportData, sizeof(reportData));
+}
--- examples/phoenix-arduino-test/phoenix-arduino-test.ino
+++ examples/phoenix-arduino-test/phoenix-arduino-test.ino
@@ -1,23 +1,3 @@
-/*
- * SN74HC165N_shift_reg
- *
- * Program to shift in the bit values from a SN74HC165N 8-bit
- * parallel-in/serial-out shift register.
- *
- * This sketch demonstrates reading in 16 digital states from a
- * pair of daisy-chained SN74HC165N shift registers while using
- * only 4 digital pins on the Arduino.
- *
- * You can daisy-chain these chips by connecting the serial-out
- * (Q7 pin) on one shift register to the serial-in (Ds pin) of
- * the other.
- *
- * Of course you can daisy chain as many as you like while still
- * using only 4 Arduino pins (though you would have to process
- * them 4 at a time into separate unsigned long variables).
- *
- * https://playground.arduino.cc/Code/ShiftRegSN74HC165N
-*/
 #include "hc165.h"
 #include "Potentiometer.h"
 
